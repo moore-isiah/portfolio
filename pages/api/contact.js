992ecf79bded9c4678a92524927ea233e21c517f
Isiah Moore <isiahistj@gmail.com>
2023-02-27 08:49:21 -0700

    maybe

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -4,8 +4,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: "isiahmdev@gmail.com",
+    pass: "jziubvclctdyntqr",
   },
 });
 
